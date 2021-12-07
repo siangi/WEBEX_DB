0523c4bf28d9dfca672ad0339885197f507c5b4f
siangi <siangi@gmx.ch>
2021-12-07 10:03:45 +0100

    My First Controller <3 Photos are now also included in the serve

---
+++ WebApplication1/Controllers/PhotoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using ArchiveAPI.Models;
+
+namespace ArchiveAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public PhotoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("ArchiveConnection"));
+
+            var dbList = dbClient.GetDatabase("Exhibitions").GetCollection<Photo>("Photos").AsQueryable();
+
+            return new JsonResult(dbList);
+        }
+    }
+}
---
+++ WebApplication1/Models/Photo.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+
+namespace ArchiveAPI.Models
+{
+    public class Photo
+    {
+        public const string PHOTO_URL = "http://localhost:49146/Photos/";
+        public ObjectId Id { get; set; }
+        public int PhotoId { get; set; }
+        public string Filename { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public string Link { get { return PHOTO_URL + Filename;  } }
+
+    }
+}
