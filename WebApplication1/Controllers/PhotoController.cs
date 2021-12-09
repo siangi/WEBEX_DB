32f359b8407beb64ba5eb0b02a5ab3997db0f5a6
siangi <siangi@gmx.ch>
2021-12-09 13:33:51 +0100

    We can now delete Databse Entries and upload photo files

--- WebApplication1/Controllers/PhotoController.cs
+++ WebApplication1/Controllers/PhotoController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
+using System.IO;
 using ArchiveAPI.Models;
 
 
@@ -12,9 +14,11 @@ namespace ArchiveAPI.Controllers
     public class PhotoController : ControllerBase
     {
         private readonly IConfiguration _configuration;
-        public PhotoController(IConfiguration configuration)
+        private readonly IWebHostEnvironment _env;
+        public PhotoController(IConfiguration configuration, IWebHostEnvironment env)
         {
             _configuration = configuration;
+            _env = env;
         }
 
         public MongoClient createStandardMongoClient()
@@ -57,6 +61,30 @@ namespace ArchiveAPI.Controllers
             return new JsonResult("Added Succesfully");
         }
 
+        [Route("SaveFile")]
+        [HttpPost]
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _env.ContentRootPath + "/Photos/" + filename;
+
+                using (var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+
+                return new JsonResult("File: " + filename + " was uploaded succesfully");
+            }
+            catch
+            {
+                return new JsonResult("there was a problem with saving the sent file");
+            }
+        }
+
         [HttpPut]
         public JsonResult Put(Photo photo)
         {
@@ -71,6 +99,19 @@ namespace ArchiveAPI.Controllers
             dbClient.GetDatabase("Exhibitions").GetCollection<Photo>("Photos").UpdateOne(filter, update);
 
             return new JsonResult("Updated Succesfully");
+        }
+
+        [HttpDelete("{PhotoId:int}")]
+        public JsonResult Delete(int photoID)
+        {
+            MongoClient dbClient = createStandardMongoClient();
+
+            var filter = Builders<Photo>.Filter.Eq("PhotoId", photoID);
+
+            var result = dbClient.GetDatabase("Exhibitions").GetCollection<Photo>("Photos").DeleteOne(filter);
+
+            return new JsonResult(result);
+
         }
 
     }
